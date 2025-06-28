9ff19ed5f6d1cf34e181c18288ab908bd0039a5f
Henrijs Kravals <henrijskravals@gmail.com>
2025-06-28 21:54:49 +0300

    fix: add #initialize

--- app/vending_machine.rb
+++ app/vending_machine.rb
@@ -5,6 +5,14 @@ require_relative 'display_manager'
 
 class VendingMachine
   attr_accessor :products
+  attr_reader :coin_manager, :product_catalog, :transaction_processor, :display_manager
+
+  def initialize
+    @coin_manager = CoinManager.new
+    @product_catalog = ProductCatalog.new
+    @transaction_processor = TransactionProcessor.new
+    @display_manager = DisplayManager.new(@transaction_processor)
+  end
 
   def insert(amount)
     @coin_manager.add_coins(amount)
